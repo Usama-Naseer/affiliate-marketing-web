9a89a8757d22dbde8c710af8bb4f46264fd20c16
Usama-Naseer <usama0naseer@gmail.com>
2023-04-09 01:06:33 +0500

    store page

--- lib/screens/store_page.dart
+++ lib/screens/store_page.dart
@@ -17,15 +17,219 @@ class _StorePageState extends State<StorePage> {
             horizontal: MediaQuery.of(context).size.width * 0.1),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
+          children: [
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text('Discount&Codes',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w400,
+                        decoration: TextDecoration.lineThrough,
+                        color: Colors.green,
+                        fontStyle: FontStyle.italic)),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Search stores and deals',
+                        contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(color: Colors.grey)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(color: Colors.grey)),
+                        suffixIcon: Container(
+                          height: 40,
+                          width: 40,
+                          padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.green),
+                          child: const Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Promo codes',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Categories',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Blogs',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 16),
+                    ),
+                  ],
+                )
+              ],
+            ),
             SizedBox(
               height: 20,
             ),
-            Center(
+            const Center(
                 child: Text(
               '35% Off ZAGG Coupon (20 Promo Codes) April 2023',
               style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ))
+            )),
+            SizedBox(height: 10,),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 4,
+                children: List.generate(
+                  20,
+                  (index) => Container(
+                    margin: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 20),
+                    decoration:  BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(12),boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: const Offset(0.5, 0.5),
+                        blurRadius: 2,
+                      )
+                    ]),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  '20% off',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 18),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5, vertical: 5),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  decoration: BoxDecoration(
+                                      color: Colors.orange.withOpacity(0.20),
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: const Center(
+                                      child: Text(
+                                    'Top Coupon',
+                                    style: TextStyle(color: Colors.orange),
+                                  )),
+                                ),
+                                const Spacer(),
+                                Image.asset(
+                                  'assets/images/meow.png',
+                                  height: 50,
+                                  width: 80,
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            RichText(
+                                text: const TextSpan(children: [
+                              TextSpan(
+                                  text: 'Southwest airlien coupon code ',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600)),
+                              TextSpan(
+                                  text:
+                                      'Earn upto 24000 points on southwest arline')
+                            ]))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Divider(
+                          height: 0.7,
+                          thickness: 0.7,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              height: 50,
+                              color: Colors.grey.shade300.withOpacity(0.4),
+                              padding: const EdgeInsets.only(left:30,right: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  const Text('FREE-SHIP-25',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600)),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 20),
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: const Center(
+                                        child: Text(
+                                      'Copy',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w400),
+                                    )),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Divider(height: 0.7,thickness: 0.7,),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
